e15a54fa5921832513acd92eb9ba1408e4f19943
B-F-1-0 <jluc1485410@gmail.com>
2024-11-02 15:15:11 -0400

    Saving changes

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // create a div element and iterate 16x16 times
 const square = document.createElement('div');
+square.className = "colorsquare";
 square.style.border = "1px solid black";
-square.style.height = "10px";
-square.style.width = "10px";
+square.style.height = "16px";
+square.style.width = "16px";
+square.style.backgroundColor = 'transparent';
+
+square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = 'black';
+});
 
 for (let i = 0; i < (16*16); i++)
 {
