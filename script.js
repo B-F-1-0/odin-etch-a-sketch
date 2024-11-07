232a6107197e97a5ef110e5f7b81076fd96e7718
B-F-1-0 <jluc1485410@gmail.com>
2024-11-07 11:46:22 -0500

    Color added

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ square.style.height = "32px";
 square.style.width = "32px";
 square.style.backgroundColor = 'transparent';
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 function colorSquare(target)
 {
-    target.style.backgroundColor = 'black';
-    console.log("hover detected and square colored successfully");
+    target.style.backgroundColor = getRandomColor();
 }
 
 for (let i = 0; i < (16*16); i++)
